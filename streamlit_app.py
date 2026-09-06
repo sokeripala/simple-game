64523cc3bb93390a9f95430fed314dac46a0422f
sokeripala <s.erkinheimo@gmail.com>
2026-09-06 17:50:01 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # we are making a basic calculator today
 
-print("Today we will do a test. Let's see if you can pass it.")
+import streamlit as st
+st.write("Today we will do a test. Let's see if you can pass it.")
 
 
 
