d8b257d3f64bd573a01ecb5b9a0647c352b2226f
sokeripala <s.erkinheimo@gmail.com>
2026-09-06 15:01:01 +0000

    5

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,17 @@
-# we are making a basic calculator today
+
 
 import pkgutil
 import streamlit as st
 import time
 
+time.sleep(3)
 st.write("Today we will do a test. Let's see if you can pass it.")
 
-time.sleep(2)
+time.sleep(3)
 
 st.write("I'm gonna make this one so hard for you.")
+
+time.sleep(3)
+
+st.write("We can do it by playing a game.")
 
