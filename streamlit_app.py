968c72412ac2ce6066af9a02d1ed22d12ee3172b
sokeripala <s.erkinheimo@gmail.com>
2026-09-06 17:53:17 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,10 @@
 # we are making a basic calculator today
 
 import streamlit as st
+import time
+
 st.write("Today we will do a test. Let's see if you can pass it.")
+
 time.sleep(2)
 
 st.write("I'm gonna make this one so hard for you.")
