e4f04ec39efc2a05c3d0734721002d827907d115
sokeripala <s.erkinheimo@gmail.com>
2026-09-06 17:44:08 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,11 @@
 # we are making a basic calculator today
 
+import matplotlib.pyplot as plt
+import tkinter as tk
+
+window = tk.Tk()
+
+window.title("calculator")
+window.geometry("300x400")
+
+window.mainloop()
